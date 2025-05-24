65b49799d311ab790e21500223c04ba6fdb3781e
Aaryan5566 <arihansharma5467@gmail.com>
2025-05-24 18:41:10 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -100,13 +100,18 @@ async def start(client, message):
             ],[
                 InlineKeyboardButton('✇ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ✇', url=CHNL_LNK)
                 ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_photo(
-            photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
-            reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML,
-            reply_to_message_id=message.id
+        m = await message.reply_sticker("CAACAgIAAxkBAAISdGZdq8CuixMcWDLSnuo0AczMCqMVAAKrAQACEBptIni-vpHMVGzHHgQ")
+await asyncio.sleep(1.5)
+await m.delete()
+
+reply_markup = InlineKeyboardMarkup(buttons)
+await message.reply_photo(
+    photo=random.choice(PICS),
+    caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+    reply_markup=reply_markup,
+    parse_mode=enums.ParseMode.HTML,
+    reply_to_message_id=message.id
+
         )
         return
         
