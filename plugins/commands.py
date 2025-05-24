e79bda7a216651b727bd06b87f468890ef5c1c22
Aaryan5566 <arihansharma5467@gmail.com>
2025-05-24 18:07:59 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -101,14 +101,19 @@ async def start(client, message):
                 InlineKeyboardButton('✇ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ✇', url=CHNL_LNK)
                 ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_photo(
-            photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
-            reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML,
-            reply_to_message_id=message.id   
-        )
-        return
+
+m = await message.reply_sticker("CAACAgIAAxkBAAISdGZdq8CuixMcWDLSnuo0AczMCqMVAAKrAQACEBptIni-vpHMVGzHHgQ")
+await asyncio.sleep(1.5)  # थोड़ा टाइम देना जरूरी है delete से पहले
+await m.delete()
+
+await message.reply_photo(
+    photo=random.choice(PICS),
+    caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+    reply_markup=reply_markup,
+    parse_mode=enums.ParseMode.HTML,
+    reply_to_message_id=message.id
+)
+return
         
      if message.command[1].startswith("reff_"):
         try:
