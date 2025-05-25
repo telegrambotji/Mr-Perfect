62601a942be3832111ee48a5b50edbb43106b9e6
Aaryan5566 <arihansharma5467@gmail.com>
2025-05-25 20:52:11 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1274,7 +1274,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ],[
                 InlineKeyboardButton('〄 Hᴇʟᴘ', callback_data='features'),
                 InlineKeyboardButton('⍟ Aʙᴏᴜᴛ', callback_data='bot'),
-                InlineKeyboardButton(' ⎊ Tʀᴇɴᴅɪɴɢㅤ', callback_data="topsearch")
+                InlineKeyboardButton('  ⎊ Tʀᴇɴᴅɪɴɢㅤ', callback_data="topsearch")
             ],[
                 InlineKeyboardButton('✇ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ✇', url=CHNL_LNK)
                 ]]
