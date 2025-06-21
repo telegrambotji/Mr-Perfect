6c0be0d1896082e3b76caadbe0ddcd5c7161f9c9
telegrambotji <telegrambotji@gmail.com>
2025-06-21 11:28:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,8 +15,8 @@ def is_enabled(value, default):
 
 
 SESSION = environ.get('SESSION', 'media_search')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
+API_ID = int(environ.get('API_ID', '20438694'))
+API_HASH = environ.get('API_HASH', '97de1430c7d6fc190135b3002a621001')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
