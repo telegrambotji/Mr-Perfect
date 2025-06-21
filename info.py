7a6cf0ab8e05df85f230df3c32647119dec51dd5
telegrambotji <telegrambotji@gmail.com>
2025-06-22 01:57:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,8 +42,8 @@ REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_
 support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002749179475') 
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://yashdd:yashdd@yashdd.jk0exmg.mongodb.net/?retryWrites=true&w=majority&appName=yashdd")
-DATABASE_NAME = environ.get('DATABASE_NAME', "yashdd")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://yashdd:yashdd@cluster0.1kn6zcl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'SilentXBotz_files')
 
 # If MULTIPLE_DB Is True Then Fill DATABASE_URI2 Value Else You Will Get Error.
